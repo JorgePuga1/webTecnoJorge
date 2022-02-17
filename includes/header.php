3b382e553c7e75a2b43cfc667d58654f4b467eec
JorgePuga1 <jorgeluisp96@hotmail.com>
2022-02-16 23:43:38 -0500

    agregando enlace en logo

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@
                 
         <nav class="navegacion">
         <div class="icono">
+                <a href="index.php">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-affiliate" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M5.931 6.936l1.275 4.249m5.607 5.609l4.251 1.275" />
@@ -25,7 +26,7 @@
                     <circle cx="18.5" cy="18.5" r="1.5" />
                     <circle cx="8.5" cy="15.5" r="4.5" />
                     </svg>
-                   
+                 </a>
                 </div>
                     <a  href="index.php" >Inicio</a>
                     <a href="soporte.php" >Soporte</a>
