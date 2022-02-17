bafc13c25ffc973c552ac03ebce274c6d72954e7
JorgePuga1 <jorgeluisp96@hotmail.com>
2022-02-17 03:01:33 -0500

    arreglando la presentacion

--- index.php
+++ index.php
@@ -11,15 +11,14 @@
                 <h1>Tecno - Jorge</h1>
                 <h3>Soluciones tecnólogicas</h3>
             </div>
-           <div class="presentacion_galeria">
-               <div class="foto1">
-               <a href="soporte.php"></a>
+           <div class="presentacion_nosotros">
+               <div class="mision">
+                   <h2>Misión</h2>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sint debitis ullam cumque, illum quod expedita numquam in a? Placeat beatae dicta molestias animi ipsa eligendi inventore maiores, aspernatur deserunt.</p>
                </div>
-               <div class="foto2">
-                   
-               </div>
-               <div class="foto3">
-                   
+               <div class="vision">
+                   <h2>Visión</h2>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sint debitis ullam cumque, illum quod expedita numquam in a? Placeat beatae dicta molestias animi ipsa eligendi inventore maiores, aspernatur deserunt.</p>
                </div>
            </div>
         </div>
